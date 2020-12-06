ae12f419acfa0e70e21cc2ccbf37a65e08dbce64
Daniel Yohan <dyohan9@gmail.com>
2020-12-05 23:56:36 -0300

    fix category

--- rookly/api/v1/category/serializers.py
+++ rookly/api/v1/category/serializers.py
@@ -9,14 +9,14 @@ from rookly.common.models import (
 class SubCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = SubCategory
-        fields = ["id", "name", "description"]
+        fields = ["id", "name"]
         ref_name = None
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ["id", "name", "description", "subcategory"]
+        fields = ["id", "name", "subcategory"]
         ref_name = None
 
     subcategory = SubCategorySerializer(many=True)
