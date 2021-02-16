696578bd1fdc026f728ad79aac3d61f1b9776b6e
Daniel Yohan <dyohan9@gmail.com>
2021-02-16 15:22:56 -0300

    Added tests unit business

--- rookly/api/v1/tests/test_business.py
+++ rookly/api/v1/tests/test_business.py
@@ -1,13 +1,15 @@
 import json
 
 from django.test import RequestFactory, TestCase
+from django.test.client import MULTIPART_CONTENT
 from rest_framework import status
 
 from rookly.api.v1.business.views import BusinessViewSet
 from rookly.api.v1.tests.utils import create_user_and_token
+from rookly.common.models import Business, City
 
 
-class CreateRepositoryAPITestCase(TestCase):
+class CreateBusinessAPITestCase(TestCase):
     fixtures = [
         "./rookly/common/fixtures/categories.json",
         "./rookly/common/fixtures/states.json",
@@ -50,3 +52,102 @@ class CreateRepositoryAPITestCase(TestCase):
 
         self.assertEqual(business.name, content_data.get("name"))
         self.assertEqual(business.cpf_cnpj, "00000000000")
+
+
+class UpdateBusinessTestCase(TestCase):
+    fixtures = [
+        "./rookly/common/fixtures/categories.json",
+        "./rookly/common/fixtures/states.json",
+    ]
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+        self.owner, self.owner_token = create_user_and_token("owner")
+        self.user, self.user_token = create_user_and_token("user")
+        self.business = Business.objects.create(
+            user=self.owner,
+            name="rookly",
+            city=City.objects.all().first(),
+            cpf_cnpj="00000000000",
+            presentation="",
+        )
+
+    def request(self, business, data={}, token=None):
+        authorization_header = (
+            {"HTTP_AUTHORIZATION": "Token {}".format(token.key)} if token else {}
+        )
+
+        request = self.factory.patch(
+            "/v1/business/control/{}/".format(business.uuid),
+            self.factory._encode_data(data, MULTIPART_CONTENT),
+            MULTIPART_CONTENT,
+            **authorization_header,
+        )
+
+        response = BusinessViewSet.as_view({"patch": "update"})(
+            request, uuid=business.uuid, partial=True
+        )
+        response.render()
+        content_data = json.loads(response.content)
+        return (response, content_data)
+
+    def test_okay_update_name(self):
+        response, content_data = self.request(
+            self.business,
+            {"name": "Rookly {}".format(self.business.uuid)},
+            self.owner_token,
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_unauthorized(self):
+        response, content_data = self.request(
+            self.business,
+            {"name": "Rookly {}".format(self.business.uuid)},
+            self.user_token,
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class DestroyBusinessTestCase(TestCase):
+    fixtures = [
+        "./rookly/common/fixtures/categories.json",
+        "./rookly/common/fixtures/states.json",
+    ]
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+        self.owner, self.owner_token = create_user_and_token("owner")
+        self.user, self.token = create_user_and_token()
+
+        self.business = Business.objects.create(
+            user=self.owner,
+            name="rookly",
+            city=City.objects.all().first(),
+            cpf_cnpj="00000000000",
+            presentation="",
+        )
+
+    def request(self, token):
+        authorization_header = {"HTTP_AUTHORIZATION": "Token {}".format(token)}
+        request = self.factory.delete(
+            "/v1/business/control/{}/".format(str(self.business.uuid)),
+            **authorization_header,
+        )
+        response = BusinessViewSet.as_view({"delete": "destroy"})(
+            request, uuid=self.business.uuid
+        )
+        response.render()
+        return response
+
+    def test_okay(self):
+        response = self.request(self.owner_token.key)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_private_okay(self):
+        response = self.request("")
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
